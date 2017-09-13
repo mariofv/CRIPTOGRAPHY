013753b18aeb4e3e0ea4c60c426ad638d5a946cf
mario.fernandez <mario.fernandez@est.fib.upc.edu>
2017-09-13 16:09:50 +0200

    Not sure

--- src/VigenereDecrypter.java
+++ src/VigenereDecrypter.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,6 +8,13 @@ import java.util.regex.Pattern;
 public class VigenereDecrypter {
 
     private String cryptogram;
+    private String filteredCryptogram;
+    private Map<Integer, Character> nonAlphabetical;
+    private ArrayList<String > mostCommonSubstrings;
+    private ArrayList<ArrayList<Integer>> possibleSizesDivisots;
+    private int size;
+    private ArrayList<String> possibleKeys;
+    private ArrayList<String > decyphedTexts;
     private String key;
     private String decryptedText;
 
@@ -16,6 +23,202 @@ public class VigenereDecrypter {
     }
 
     public void run() {
+        filterCryptogram();
+        findMostCommonSubstrings();
+        findPossibleSizes();
+        findSize();
+        generatePossibleKeys();
+        computeDecyphedTexts();
+        getAwnser();
+    }
+
+    private void filterCryptogram() {
+        filteredCryptogram = "";
+        nonAlphabetical = new HashMap<>();
+        int j = 0;
+        for (int i = 0; i < cryptogram.length(); ++i) {
+            char c = cryptogram.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                filteredCryptogram += c;
+                ++j;
+            }
+            else {
+                nonAlphabetical.put(j,c);
+            }
+        }
+    }
+
+    private void findMostCommonSubstrings() {
+        mostCommonSubstrings = new ArrayList<>();
+        //TODO: Encontrar las 3 substrings mas comunes de mas de 3 letras
+
+        Map<String, Integer> map = new HashMap<>();
+        int limit = filteredCryptogram.length() - 7 + 1;
+        for (int i = 0; i < limit; i++) {
+            String sub = filteredCryptogram.substring(i, i + 7);
+            Integer counter = map.get(sub);
+            if (counter == null) {
+                counter = 0;
+            }
+            map.put(sub, ++counter);
+        }
+        map = sortByValue(map);
+        ArrayList<Map.Entry<String,Integer>> entries = new ArrayList<>(map.entrySet());
+        int j = entries.size()-1;
+        while ( j >= 0 && mostCommonSubstrings.size() < 3) {
+            if (entries.get(j).getKey().length() > 3) {
+                mostCommonSubstrings.add(entries.get(j).getKey());
+            }
+            --j;
+        }
+    }
+
+    private Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+
+        // 1. Convert Map to List of Map
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+        // 2. Sort list with Collections.sort(), provide a custom Comparator
+        //    Try switch the o1 o2 position for a different order
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        /*
+        //classic iterator example
+        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<String, Integer> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }*/
+
+
+        return sortedMap;
+    }
+
+    private void findPossibleSizes() {
+        possibleSizesDivisots = new ArrayList<>();
+        for (int i = 0; i < mostCommonSubstrings.size(); ++i) {
+            String commonSubstring = mostCommonSubstrings.get(i);
+            ArrayList<Integer> divisors = new ArrayList<>();
+            for (int j = -1; (j = filteredCryptogram.indexOf(commonSubstring, j + 1)) != -1; j++) {
+                divisors.add(j);
+            }
+            possibleSizesDivisots.add(divisors);
+        }
+    }
+
+    private void findSize() {
+        Map<Integer, Integer> possibleSizes = new HashMap<>();
+        for (ArrayList<Integer> divisors : possibleSizesDivisots) {
+            int possibleSize = gcd(divisors);
+            if (!possibleSizes.containsKey(possibleSize)) {
+                possibleSizes.put(possibleSize, 1);
+            }
+            else {
+                possibleSizes.put(possibleSize, possibleSizes.get(possibleSize) + 1);
+            }
+        }
+        int max = -1;
+        for (Map.Entry<Integer, Integer> x : possibleSizes.entrySet()) {
+            if (x.getValue() > max) {
+                size = x.getKey();
+                max = x.getValue();
+            }
+        }
+    }
+
+    private int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private int gcd(ArrayList<Integer> divisors) {
+        int result = divisors.get(0);
+        for(int i = 1; i < divisors.size(); i++) result = gcd(result, divisors.get(i));
+        return result;
+    }
+
+    private void generatePossibleKeys() {
+        possibleKeys = new ArrayList<>();
+        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        iterate(chars, new char[size], 0);
+    }
+
+    private void iterate(char[] chars, char[] build, int pos) {
+        if (pos == size) {
+            possibleKeys.add(new String(build));
+            return;
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            build[pos] = chars[i];
+            iterate(chars, build, pos + 1);
+        }
+    }
+
+    private void computeDecyphedTexts() {
+        decyphedTexts = new ArrayList<>();
+        for (int i = 0; i < possibleKeys.size(); ++i) {
+            String possibleKey = possibleKeys.get(i);
+            int actKeyChar = 0;
+            String decryptedText = "";
+            for (int j = 0; j < filteredCryptogram.length(); ++j) {
+                char c = filteredCryptogram.charAt(j);
+                if (nonAlphabetical.containsKey(j)) {
+                    decryptedText += nonAlphabetical.get(j);
+                }
+                char decryptedLetter = (char)(c - possibleKey.charAt(actKeyChar));
+                if (c >= 'A' && c <= 'Z') {
+                    if (decryptedLetter < 'A') decryptedLetter = (char)(decryptedLetter + 26);
+                }
+                else {
+                    if (decryptedLetter < 'a') decryptedLetter = (char)(decryptedLetter + 26);
+                }
+                decryptedText += decryptedLetter;
+                ++actKeyChar;
+                if (actKeyChar >= possibleKey.length()) actKeyChar = 0;
+            }
+            decyphedTexts.add(decryptedText);
+        }
+    }
+
+    private void getAwnser() {
+        int max = -1;
+        int numKey = -1;
+        for (int i = 0; i < decyphedTexts.size(); ++i) {
+            int act = checkApperances(i);
+            if (act > max) {
+                numKey = i;
+                max = act;
+            }
+        }
+        key = possibleKeys.get(numKey);
+        decryptedText = decyphedTexts.get(numKey);
+    }
+
+    private int checkApperances(int i) {
+        int j = 0;
+        String str = decyphedTexts.get(i).toLowerCase();
+        Pattern p = Pattern.compile("the");
+        Matcher m = p.matcher( str );
+        while (m.find()) {
+            j++;
+        }
+        return j; // Prints 2
     }
 
 
