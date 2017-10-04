bed38fd5cbacef7a0368cebfda729359d75b7b92
Mario Fernandez Villalba <mario_bcn2@hotmail.es>
2017-10-04 10:47:40 +0200

    Part 1 finished

--- P2/GFFunctions.py
+++ P2/GFFunctions.py
@@ -1,4 +1,6 @@
 from fractions import gcd
+from random import randint
+import time
 
 def multPerX(a):
     result = (a << 1)&0xFF
@@ -31,10 +33,10 @@ def GF_tables():
     return (exponencial, logaritme)
 
 
-def GF_product_t(a, b):
+def GF_product_t(a, b, exponencial, logaritme):
     if a == 0 or b == 0:
         return 0
-    [exponencial, logaritme] = GF_tables()
+    #[exponencial, logaritme] = GF_tables()
     return exponencial[(logaritme[a] + logaritme[b])%256]
         
 def GF_generador():
@@ -50,4 +52,154 @@ def GF_invers(a):
         return 0
     [exponencial, logaritme] = GF_tables()
     return exponencial[255-logaritme[a]]
+
+
+[exponencial, logaritme] = GF_tables()
+with open('GF_product_p_vs_GF_product_t.txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = randint(0, 255)
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+with open('GF_product_p(a,0x02)_vs_GF_product_t(a,0x02).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x02
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+    
+with open('GF_product_p(a,0x03)_vs_GF_product_t(a,0x03).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x03
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+with open('GF_product_p(a,0x09)_vs_GF_product_t(a,0x09).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x09
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+with open('GF_product_p(a,0x0B)_vs_GF_product_t(a,0x0B).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x0B
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+with open('GF_product_p(a,0x0D)_vs_GF_product_t(a,0x0D).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x0D
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+with open('GF_product_p(a,0x0E)_vs_GF_product_t(a,0x0E).txt','w') as f:
+    f.write("{:<15}{:<15}{:<30}{:<50}\n".format('a','b',"GF_product_p","GF_product_t"))
+    [exponencial, logaritme] = GF_tables()
+    for i in range(1,1000):
+        a = randint(0, 255)
+        b = 0x0E
+        
+        start = time.time()
+        GF_product_p(a,b)
+        end = time.time()
+        timeMicroP = (end - start) * 10**6
+        
+        start = time.time()
+        GF_product_t(a,b,exponencial,logaritme)
+        end = time.time()
+        timeMicroT = (end - start) * 10**6
+        
+        f.write(("{:<15}{:<15}{:<30}{:<50}\n").format(a,b, timeMicroP, timeMicroT))
+
+f.closed
+
+
 
